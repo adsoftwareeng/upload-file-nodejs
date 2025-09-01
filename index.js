47420443a8fcef439fc9d29b373b7f2f487184ab
adsoftwareeng <a.d.softwareeng@gmail.com>
2025-09-01 23:34:16 +0530

    Initial commit - file updated

--- index.js
+++ index.js
@@ -3,21 +3,21 @@ const multer  = require('multer');
 const path = require('path');
 const app = express(); 
 
-const upload = multer({
-      storage:multer.diskStorage({
-        destination:function(req,file,cb){
-             cb(null,"uploads");
-        },
-        filename:function(req,file,cb){
-          // console.log(file);
-          let fileEXT = path.extname(file.originalname);
-           cb(null, file.fieldname + "-" + Date.now() +fileEXT)
-        }
-      })
-}).single('file');
-
-app.post("/upload", upload, (req,res)=>{
-  res.send("File Uploaded");
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null,"uploads");
+  },
+  filename: (req, file, cb) => {
+    let fileEXT = path.extname(file.originalname);
+    cb(null, file.fieldname + "-" + Date.now() + fileEXT);
+  }
 });
+
+const upload = multer({ storage }).array('file', 10); 
+
+app.post("/upload", upload, (req, res) => {
+  res.send(`File Uploaded successfully`);
+});
+
 
 app.listen(5000);
